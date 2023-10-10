9e3ce6b25218074935626ff8a6f231578cfb248d
Noroze <106557207+Noroze@users.noreply.github.com>
2023-10-10 13:55:18 +0400

    Update Privacy.js

--- pages/Privacy.js
+++ pages/Privacy.js
@@ -8,14 +8,14 @@ const Privacy = ({ properties }) => {
   return (
     <Box>
      
-     <Text fontSize='xl' p='4' fontWeight='bold'>Privacy Policy for Starvo</Text>
+     <Text fontSize='xl' p='4' fontWeight='bold'>Privacy Policy for Bylonista</Text>
       <Text fontSize='l' p='4' >
       
-At Starvo, accessible from Starvo.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Starvo and how we use it.
+At Bylonista, accessible from Bylonista.store, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Bylonista and how we use it.
 
 If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.
 
-This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Starvo. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Free Privacy Policy Generator.
+This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Bylonista. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Free Privacy Policy Generator.
 <Text fontSize='xl' p='4' fontWeight='bold'>Consent</Text>
 
 By using our website, you hereby consent to our Privacy Policy and agree to its terms.
@@ -40,25 +40,26 @@ Find and prevent fraud
 <Text fontSize='xl' p='4' fontWeight='bold'>Log Files</Text>
 
 
-Starvo follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+Bylonista follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
 <Text fontSize='xl' p='4' fontWeight='bold'>Cookies and Web Beacons</Text>
 
-Like any other website, Starvo uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+Like any other website, Bylonista uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
 <Text fontSize='xl' p='4' fontWeight='bold'>Google DoubleClick DART Cookie</Text>
 
 
 Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads
 <Text fontSize='xl' p='4' fontWeight='bold'>Advertising Partners Privacy Policies</Text>
 
-You may consult this list to find the Privacy Policy for each of the advertising partners of Starvo.
+You may consult this list to find the Privacy Policy for each of the advertising partners of Bylonista.
 
-Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Starvo, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Bylonista, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
 
-Note that Starvo has no access to or control over these cookies that are used by third-party advertisers.
+Note that Bylonista has no access to or control over these cookies that are used by third-party advertisers.
 <Text fontSize='xl' p='4' fontWeight='bold'>Third Party Privacy Policies</Text>
 
 
-Starvo's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+Bylonista
+  's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
 
 You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
 <Text fontSize='xl' p='4' fontWeight='bold'>CCPA Privacy Rights (Do Not Sell My Personal Information)</Text>
