fca298bf3b8de2829b4fae2df969e61554a4e2cb
Noroze <106557207+Noroze@users.noreply.github.com>
2023-10-10 13:54:24 +0400

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -10,7 +10,7 @@ const About = ({ properties }) => {
      
       
       <Text fontSize='xl' p='4' fontWeight='bold'>
-        Starvo is the leading property website in the UAE connecting buyers, sellers and tenants, so that every real estate requirement in the region is fulfilled in a seamless, user-friendly manner.Going above and beyond just providing a database of available listings for rent and sale, Starvo has taken the onus of becoming the most reliable source for ‘all things real estate’. In the UAE today, every question that a potential end user could encounter is answered through the resources Starvo has created. {router.query.purpose}
+        Bylonista is the leading property website in the UAE connecting buyers, sellers and tenants, so that every real estate requirement in the region is fulfilled in a seamless, user-friendly manner.Going above and beyond just providing a database of available listings for rent and sale, Starvo has taken the onus of becoming the most reliable source for ‘all things real estate’. In the UAE today, every question that a potential end user could encounter is answered through the resources Starvo has created. {router.query.purpose}
       </Text>
    
     </Box>
